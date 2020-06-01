4f7e19bba7b13dd877d7aad2acd0dfd2791e3e05
k018c1052 <k018c1052@it-neec.jp>
2020-06-01 13:26:15 +0900

    up2

--- C-2_2.py
+++ C-2_2.py
@@ -1,11 +1,5 @@
 num = int(input('番号＞'))
 moji = ' ABCDEFGHIJKLNMOPQRSTUVWXYZ'
 
-if num < 27:
-    print(moji[num])
-    pass
-
-if num >= 27:
-    num %= 27
-    print(moji[num])
-    pass
+num %= 27
+print(moji[num])
